72d27da03219af2b19762fdc44b940d60ecd8ced
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-09-02 12:07:22 +0200

    RMX1971: Don't build vndk_package
    * It's built by default if vndk is set to current
    
    Change-Id: I2a00446046bafe265e5ac894656abc72514319d8

--- device.mk
+++ device.mk
@@ -466,10 +466,6 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-service \
     android.hardware.vibrator@1.0-impl
 
-# VNDK-SP
-PRODUCT_PACKAGES += \
-    vndk_package
-
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
