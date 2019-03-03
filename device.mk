a1e728626b8bf17bc62f9f32c7a7ed76b11a5773
HarukeyUA <inazarrusnak@gmail.com>
2019-03-03 10:19:54 +0000

    RMX1971: Improve Bluetooth + Wi-Fi Coexistence
    Thanks to silent27121984 for fix
    
    Signed-off-by: smokey18 <kmboplays@gmail.com>

--- device.mk
+++ device.mk
@@ -136,13 +136,8 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth.audio@2.0-impl \
-    audio.bluetooth.default \
-    BluetoothResCommon \
-    libbthost_if \
-    libldacBT_dec \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
-    vendor.qti.hardware.btconfigstore@1.0.vendor
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    libbt-vendor
 
 # Binder
 PRODUCT_PACKAGES += \
