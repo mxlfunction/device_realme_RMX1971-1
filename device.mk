2b70aa02be0646e34bc935783828eb179b768607
Testobuzz <68677930+TestoBuzz@users.noreply.github.com>
2020-09-17 13:46:57 +0530

    RMX1971:Remove more Lineage stuff we dont use

--- device.mk
+++ device.mk
@@ -467,10 +467,6 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-service \
     thermal.sdm710
 
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
