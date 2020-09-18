895c0c8d9f4599e380c2f75445e0d2476c6dad76
Testobuzz <68677930+TestoBuzz@users.noreply.github.com>
2020-09-18 17:05:36 +0530

    RMX1971:Add Launch Choices

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_RMX1971.mk
+    $(LOCAL_DIR)/havoc_RMX1971.mk
+
+COMMON_LUNCH_CHOICES := \
+    zenx_RMX1971-user \
+    zenx_RMX1971-userdebug \
+    zenx_RMX1971-eng
