58159dab5d919581ff102ffa84de451e91bdd0d4
Danny Lin <danny@kdrag0n.dev>
2020-09-18 22:47:02 -0700

    RMX1971: Switch Skia rendering backend to Vulkan
    This results in slightly lower frame times in most UiBench tests:
    
    Skia + Adreno OpenGL:
                                Score   Jank Frames Bad Frames  Avg Frame Time (ms)
    Overdraw Test               1       0.01%       0.06%       3.16
    Edit Text Input             5       0.13%       4.99%       7.79
    High-hitrate text render    1       0.01%       0.51%       6.81
    Image List View Fling       1       0.01%       0.32%       6.73
    Bitmap Upload Test          0       0.01%       0.99%       9.38
    Low-hitrate text render     1       0.01%       0.49%       7.08
    List View Fling             23      0.12%       0.78%       6.61
    Shadow Grid Fling           16      0.10%       0.94%       7.32
    
    Skia + Adreno Vulkan:
                                Score   Jank Frames Bad Frames  Avg Frame Time (ms)
    Overdraw Test               1       0.01%       0.06%       2.94
    Edit Text Input             6       0.13%       4.60%       8.00
    High-hitrate text render    1       0.01%       0.28%       6.29
    Image List View Fling       1       0.01%       0.36%       5.96
    Bitmap Upload Test          1       0.01%       0.15%       9.25
    Low-hitrate text render     1       0.01%       0.33%       6.71
    List View Fling             16      0.09%       2.70%       6.62
    Shadow Grid Fling           11      0.07%       0.49%       6.98
    
    Change-Id: Idb23fca4d801746588a7ba63231705a7f9621b35
    Signed-off-by: Harshit Jain <god@hyper-labs.tech>

--- product_prop.mk
+++ product_prop.mk
@@ -8,3 +8,7 @@
 # Default to BFQ I/O scheduler
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.io.scheduler=cfq
+
+# Use Vulkan backend for Skia UI rendering
+PRODUCT_PRODUCT_PROPERTIES += \
+    debug.hwui.renderer=skiavk
