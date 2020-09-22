440c34566d33108d12d4927b55bda13333f30fdd
Harshit Jain <god@hyper-labs.tech>
2020-09-23 05:09:32 +0530

    RMX1971: set display density via product prop
    * product/build.prop is (supposedly) loaded after vendor and system build.prop
    * On R display desity isn't getting set for weird reason so override it here to make sure it gets set.
    
    Signed-off-by: Harshit Jain <god@hyper-labs.tech>

--- product_prop.mk
+++ product_prop.mk
@@ -9,6 +9,10 @@
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.io.scheduler=cfq
 
+# Override Display Density
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.sf.lcd_density=420
+
 # Use Vulkan backend for Skia UI rendering
 PRODUCT_PRODUCT_PROPERTIES += \
     debug.hwui.renderer=skiavk
