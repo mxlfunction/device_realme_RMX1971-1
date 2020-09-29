f5fbf205536841704caf4f4dbb39759d81ae1131
Aoihara <veronicadisa6@gmail.com>
2020-09-29 14:51:29 +0000

    test2

--- device.mk
+++ device.mk
@@ -427,6 +427,7 @@ PRODUCT_PACKAGES += \
     android.hardware.radio@1.4 \
     android.hardware.radio.config@1.1 \
     android.hardware.radio.deprecated@1.0 \
+    libprotobuf-cpp-full \
     librmnetctl \
     libxml2
 
