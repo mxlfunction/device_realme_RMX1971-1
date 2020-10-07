ff92bbd797673af7a3112d65d9b8d40034d42d60
lucky <ah440099@gmail.com>
2020-10-07 02:22:08 +0200

    RMX1971: switch to qti-default/uprev display config to 1.9

--- device.mk
+++ device.mk
@@ -219,7 +219,7 @@ PRODUCT_PACKAGES += \
     libtinyxml \
     libvulkan \
     memtrack.sdm710 \
-    vendor.display.config@1.7 \
+    vendor.display.config@1.9 \
     vendor.qti.hardware.display.allocator@1.0-service \
     libgui_vendor
 
