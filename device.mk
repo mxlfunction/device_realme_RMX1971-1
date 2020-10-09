ee68778e1bf04c7fb68c008935a1567cd94d04ea
703joko <703joko@gmail.com>
2020-10-09 20:59:52 +0700

    ntah

--- device.mk
+++ device.mk
@@ -389,7 +389,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power@1.3-service.RMX1971-libperfmgr
 
 # Perf
 PRODUCT_COPY_FILES += \
