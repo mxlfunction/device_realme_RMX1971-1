a3884129b27375d6f1134c5aa544c24c58faf1e8
703joko <703joko@gmail.com>
2020-10-27 15:23:25 +0700

    last fixed

--- device.mk
+++ device.mk
@@ -130,7 +130,7 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth.audio@2.1-impl \
+    android.hardware.bluetooth.audio@2.0-impl \
     audio.bluetooth.default \
     BluetoothResCommon \
     libbthost_if \
